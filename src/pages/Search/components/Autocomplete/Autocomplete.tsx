55dcdc038410878af5d78d8292a87de7e35cef65
Rafael Lopez Bobdailla <rafa_lb5@hotmail.com>
2024-03-12 18:07:11 +0100

    some

--- src/pages/Search/components/Autocomplete/Autocomplete.tsx
+++ src/pages/Search/components/Autocomplete/Autocomplete.tsx
@@ -15,7 +15,9 @@ const Autocomplete = ({ options, field, closeOverlay }: AutocompleteProps) => {
     setParams(newParams)
     if (closeOverlay !== undefined) closeOverlay()
   }
-  const value = options.find(option => option.toLowerCase() == params.get(field)?.toLowerCase())
+  const value = options.find(option =>
+    option.toLowerCase() == params.get(field)?.toLowerCase())
+  console.log('render')
   return (
     <AutocompleteMui options={options}
       value={value ? value : options[0]}
