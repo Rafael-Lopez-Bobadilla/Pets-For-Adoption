1161f481d8462c79907b20f324ed19e1298c5202
Rafael Lopez Bobdailla <rafa_lb5@hotmail.com>
2024-03-12 17:45:59 +0100

    some

--- src/pages/Search/components/Autocomplete/Autocomplete.tsx
+++ src/pages/Search/components/Autocomplete/Autocomplete.tsx
@@ -1,7 +1,6 @@
 import { Autocomplete as AutocompleteMui } from "@mui/material"
 import { TextField } from "@mui/material"
 import { useSearchParams } from "react-router-dom"
-import { useRef } from "react"
 type AutocompleteProps = {
   options: string[],
   field: string,
@@ -9,26 +8,14 @@ type AutocompleteProps = {
 }
 const Autocomplete = ({ options, field, closeOverlay }: AutocompleteProps) => {
   const [params, setParams] = useSearchParams()
-  const checkValue = useRef(true)
   const onChange = (_e: any, newValue: string | null) => {
     let newParams = new URLSearchParams(params)
     if (newValue === 'Any') newParams.delete(field)
     if (newValue !== 'Any' && newValue) newParams.set(field, newValue)
     setParams(newParams)
-    checkValue.current = false
     if (closeOverlay !== undefined) closeOverlay()
   }
-  const getValue = () => {
-    if (!params.has(field)) return undefined
-    if (!checkValue.current) {
-      checkValue.current = true
-      return params.get(field)
-    }
-    console.log('looping')
-    const value = options.find(option => option.toLowerCase() == params.get(field)?.toLowerCase())
-    return value
-  }
-  const value = getValue()
+  const value = options.find(option => option.toLowerCase() == params.get(field)?.toLowerCase())
   return (
     <AutocompleteMui options={options}
       value={value ? value : options[0]}
