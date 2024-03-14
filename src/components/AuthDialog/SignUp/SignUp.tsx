219f4fdd889462b746842d7ad91dd99cf18d34a0
Rafael Lopez Bobdailla <rafa_lb5@hotmail.com>
2024-03-14 16:34:20 +0100

    some

--- src/components/AuthDialog/SignUp/SignUp.tsx
+++ src/components/AuthDialog/SignUp/SignUp.tsx
@@ -1,7 +1,23 @@
-
+import s from './SignUp.module.css'
 const SignUp = () => {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+  }
   return (
-    <div>Sign up</div>
+    <div className={s.signup}>
+      <h3>Sign up</h3>
+      <form className={s.form} onSubmit={handleSubmit}>
+        <label>Username</label>
+        <input />
+        <label>Email</label>
+        <input />
+        <label>Password</label>
+        <input />
+        <label>Confirm Password</label>
+        <input />
+        <button>Sign Up</button>
+      </form>
+    </div>
   )
 }
 
