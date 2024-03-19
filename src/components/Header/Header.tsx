ecf805f7886964caa8a047b18ac2894bf23538c5
Rafael Lopez Bobdailla <rafa_lb5@hotmail.com>
2024-03-19 18:43:23 +0100

    added link to header name

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,15 +5,18 @@ import close from '../../assets/svgs/close.svg'
 import { useState } from 'react'
 import HeaderOptions from './components/HeaderOptions/HeaderOptions'
 import HeaderDropMenu from './components/HeaderDropMenu/HeaderDropMenu'
+import { Link } from 'react-router-dom'
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false)
   return (
     <header className={s.header}>
       <div className={s.wrapper}>
-        <div className={s.name}>
-          <img src={logo} />
-          <span>Pets For Adoption</span>
-        </div>
+        <Link to={'/search'}>
+          <div className={s.name}>
+            <img src={logo} />
+            <span>Pets For Adoption</span>
+          </div>
+        </Link>
         {!isOpen ?
           <button className={s.button}><img src={menu} onClick={() => setIsOpen(!isOpen)} /></button> :
           <button className={s.button}><img src={close} onClick={() => setIsOpen(!isOpen)} /></button>
