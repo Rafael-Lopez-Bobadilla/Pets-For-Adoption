ed98eba2ead5ff2390728ed5fc87ae1bc3280b82
Rafael Lopez Bobdailla <rafa_lb5@hotmail.com>
2024-03-22 13:08:18 +0100

    changed closeOverlay in typesSelect component to optional

--- src/pages/Search/components/SearchBar/components/TypesSelect/TypesSelect.tsx
+++ src/pages/Search/components/SearchBar/components/TypesSelect/TypesSelect.tsx
@@ -2,7 +2,7 @@ import Select from "../../../Select/Select"
 import { TypesContext } from "../../../PetsInfoProvider/PetsInfoProvider"
 import { PetType } from "../../../../utils/IPetType"
 import { useContext } from "react"
-const TypesSelect = ({ closeOverlay }: { closeOverlay: () => void }) => {
+const TypesSelect = ({ closeOverlay }: { closeOverlay?: () => void }) => {
   const types = useContext(TypesContext)
   const names = types ? types.map((type: PetType) => type.name) : null
   return (
