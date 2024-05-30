f37601caa5d6e6865923378b7f31f63755b62bcd
Rafael Lopez Bobadilla <rafael.lopez.bob@gmail.com>
2024-05-30 16:02:55 +0200

    did nothing

--- src/pages/Search/components/LocationProvider/LocationProvider.tsx
+++ src/pages/Search/components/LocationProvider/LocationProvider.tsx
@@ -1,13 +1,16 @@
-import { useState, createContext } from "react"
-import { ILocationContext, Location } from "../../utils/ILocation"
-export const LocationContext = createContext<ILocationContext>({ location: null, setLocation: () => { } })
+import { useState, createContext } from "react";
+import { ILocationContext, Location } from "../../utils/ILocation";
+export const LocationContext = createContext<ILocationContext>({
+  location: null,
+  setLocation: () => {},
+});
 const LocationProvider = ({ children }: { children: React.ReactNode }) => {
-  const [location, setLocation] = useState<Location | null>(null)
+  const [location, setLocation] = useState<Location | null>(null);
   return (
     <LocationContext.Provider value={{ location, setLocation }}>
       {children}
     </LocationContext.Provider>
-  )
-}
+  );
+};
 
-export default LocationProvider
+export default LocationProvider;
