9eb0563fb412d22ed62b4c965b09e792a0807667
Rafael Lopez Bobadilla <rafael.lopez.bob@gmail.com>
2024-06-04 16:58:04 +0200

    refactored googlebutton componente

--- src/components/AuthDialog/components/GoogleButton/GoogleButton.tsx
+++ src/components/AuthDialog/components/GoogleButton/GoogleButton.tsx
@@ -1,18 +1,33 @@
 import { useRef, useEffect } from "react";
-
+import { useUserContext } from "../../../UserProvider/UserProvider";
+import { googleAuth } from "./googleAuth";
+import { useDialogUpdaterContext } from "../../../DialogProvider/DialogProvider";
 const GoogleButton = () => {
   const googleRef = useRef<HTMLDivElement>(null);
-  useEffect(() => {
-    if (googleRef.current) {
-      google.accounts.id.renderButton(googleRef.current, {
-        theme: "filled_blue",
-        size: "medium",
-        shape: "pill",
-        type: "standard",
-        text: "continue_with",
-        width: 150,
-      });
+  const { updateUser } = useUserContext();
+  const { closeDialog } = useDialogUpdaterContext();
+  const onSignIn = async (response: google.accounts.id.CredentialResponse) => {
+    const data = await googleAuth(response);
+    if (data.status === "success") {
+      updateUser(data.user);
+      closeDialog();
     }
+  };
+  useEffect(() => {
+    google.accounts.id.initialize({
+      client_id:
+        "215994121648-uc647b3pmh2jui4pp6te812880pd5rvk.apps.googleusercontent.com",
+      callback: onSignIn,
+      use_fedcm_for_prompt: true,
+    });
+    google.accounts.id.renderButton(googleRef.current!, {
+      theme: "filled_blue",
+      size: "medium",
+      shape: "pill",
+      type: "standard",
+      text: "continue_with",
+      width: 150,
+    });
   }, []);
   return <div ref={googleRef}></div>;
 };
--- src/components/DialogProvider/DialogProvider.tsx
+++ src/components/DialogProvider/DialogProvider.tsx
@@ -1,13 +1,10 @@
 import {
   useState,
   createContext,
-  useEffect,
   useContext,
   useCallback,
   useMemo,
 } from "react";
-import { googleAuth } from "./googleAuth";
-import { useUserContext } from "../UserProvider/UserProvider";
 type TDialogContext = {
   open: boolean;
   type: string;
@@ -21,7 +18,6 @@ export const DialogUpdaterContext = createContext<TUpdaterContext | null>(null);
 const DialogProvider = ({ children }: { children: React.ReactNode }) => {
   const [open, setOpen] = useState(false);
   const [type, setType] = useState("none");
-  const { updateUser } = useUserContext();
   const closeDialog = useCallback(() => {
     setType("none");
     setOpen(false);
@@ -29,21 +25,6 @@ const DialogProvider = ({ children }: { children: React.ReactNode }) => {
   const openDialog = useCallback((type: string) => {
     setOpen(true);
     setType(type);
-  }, []);
-  const onSignIn = async (response: google.accounts.id.CredentialResponse) => {
-    const data = await googleAuth(response);
-    if (data.status === "success") {
-      updateUser(data.user);
-      closeDialog();
-    }
-  };
-  useEffect(() => {
-    google.accounts.id.initialize({
-      client_id:
-        "215994121648-uc647b3pmh2jui4pp6te812880pd5rvk.apps.googleusercontent.com",
-      callback: onSignIn,
-      use_fedcm_for_prompt: true,
-    });
   }, []);
   const updaterValue = useMemo(
     () => ({
