f634bba9ae0480b36f73581d7e49108ae19699c1
Rafael Lopez Bobadilla <rafael.lopez.bob@gmail.com>
2024-07-18 15:04:13 +0200

    added invalid pet id handling in pet page

--- src/pages/Pet/Pet.tsx
+++ src/pages/Pet/Pet.tsx
@@ -1,5 +1,5 @@
 import s from "./Pet.module.css";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { usePetfinderToken } from "../../context/TokenContext/context";
 import Photos from "./components/Photos/Photos";
 import FavButton from "../../components/FavButton/FavButton";
@@ -8,14 +8,25 @@ import { useQuery } from "@tanstack/react-query";
 import { getPet } from "../../services/petfinderService/petfinderService";
 import LoadError from "../../components/LoadError/LoadError";
 import Info from "./components/Info/Info";
+import { AxiosError } from "axios";
 const Pet = () => {
   const { token } = usePetfinderToken();
   const { id } = useParams();
+  const navigate = useNavigate();
   const getData = async () => {
     if (!id) throw new Error("No id");
     if (!token) throw new Error("No token");
-    const data = await getPet(token, id);
-    return data.animal;
+    try {
+      const data = await getPet(token, id);
+      return data.animal;
+    } catch (err) {
+      if (err instanceof AxiosError && err.response?.status === 400) {
+        navigate("/search");
+        throw new Error("Invalid pet id");
+      } else {
+        throw new Error("something went wrong");
+      }
+    }
   };
   const { data, isPending, error, refetch } = useQuery({
     queryKey: ["pet", id],
